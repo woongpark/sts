e8c1629696a4e4a5142270cd3647975ca716ce11
HahmBogak <dlacnd7747@gmail.com>
2015-09-13 05:12:41 +0900

    첫번째 speed prediction 완성
    날씨와 사고에 대한 표시는 ptime별 데이터가 없어서 테스트의 형태로 들어 있는 5대의 데이터를 활용하여 기능만 구현해 놓음
    맵에 이미지를 표시하는 path 구현 방법이 생각 안나서 지금은 엑박으로 이미지 부분이 표시됨(웅형과 수정 필요)

--- public/scripts/sts.js
+++ public/scripts/sts.js
@@ -3,23 +3,16 @@ function getPolygon(linkinfo, direct, speed) {
       y1 = linkinfo.SLON,
       x2 = linkinfo.ELAT,
       y2 = linkinfo.ELON,
-      diff,
       color;
 
-  if(direct == "S") {
-    diff = -0.01;
-  } else {
-    diff = +0.01;
-  }
-
   speed = Math.min(speed, 130);
-  speed = parseInt(speed / 130 * 255 + 0.5, 10);
+  speed = parseInt(speed / 130 * 255+0.5, 10);
   color = new Microsoft.Maps.Color(255,255-speed,0,speed);
 
   // Create a polygon
   var vertices = new Array(
-    new Microsoft.Maps.Location(x1 + diff, y1 - diff),
-    new Microsoft.Maps.Location(x2 + diff, y2 - diff)
+    new Microsoft.Maps.Location(x1, y1),
+    new Microsoft.Maps.Location(x2, y2)
   );
   var polygon = new Microsoft.Maps.Polygon(vertices,{
     fillColor: new Microsoft.Maps.Color(0,0,0,0),
@@ -27,6 +20,40 @@ function getPolygon(linkinfo, direct, speed) {
     strokeThickness: 4
   });
   return polygon;
+}
+
+function getPolygon_risk(linkinfo, direct, risk) {
+  var x1 = linkinfo.SLAT,
+      y1 = linkinfo.SLON,
+      x2 = linkinfo.ELAT,
+      y2 = linkinfo.ELON,
+      color;
+
+  risk = Math.min(risk, 2);
+  risk = parseInt(risk / 2.0 * 255+0.5, 10);
+  color = new Microsoft.Maps.Color(255,255-risk,risk,0);
+
+  // Create a polygon
+  var vertices = new Array(
+    new Microsoft.Maps.Location(x1, y1),
+    new Microsoft.Maps.Location(x2, y2)
+  );
+  var polygon = new Microsoft.Maps.Polygon(vertices,{
+    fillColor: new Microsoft.Maps.Color(0,0,0,0),
+    strokeColor: color,
+    strokeThickness: 4
+  });
+  return polygon;
+}
+
+function getIcon(TYPE, LAT, LON) {
+  // Create a icon
+  var loc = new Microsoft.Maps.Location(LAT, LON);
+  var str = TYPE + '.png';//타입별로 이미지를 선택하도록 고처야함
+  var pushpinOptions = {icon: str, width: 10, height: 10};
+  var pushpin= new Microsoft.Maps.Pushpin(loc, pushpinOptions);
+
+  return pushpin;
 }
 
 function getMap() {
@@ -46,16 +73,17 @@ function getChart1() {
   $(function () {
     $('#chartDiv1').highcharts({
       title: {
-        text: 'Link Prediction',
+        text: 'Link Prediction 1',
         x: -20 //center
       },
       subtitle: {
-        text: 'Speed and Collision Risk',
+        text: 'Speed',
         x: -20
       },
       yAxis: {
         title: {
-          text: "PSPEED"
+          text: "PSPEED",
+          min: 0.0
         }
       },
       legend: {
@@ -63,7 +91,10 @@ function getChart1() {
       },
       series: [{
         name: "PSPEED",
-        data: []
+        data: [],
+        tooltip: {
+            headerFormat: 'No : {point.key}<br/>'
+        }
       }]
     });
   });
@@ -77,142 +108,280 @@ function setChart1(data) {
   var data = $.map(data, function(obj) {
     return obj.PSPEED;
   })
-  chart.xAxis[0].setCategories(categories);
+  //chart.xAxis[0].setCategories(categories);
   chart.series[0].setData(data);
+  chart.xAxis[0].update({
+    labels: {
+      formatter: function() {
+        return categories[this.value];
+      }
+    }
+  }, true);
+}
+
+function getChart1_risk() {
+  $(function () {
+    $('#chartDiv1_risk').highcharts({
+      title: {
+        text: 'Link Prediction 2',
+        x: -20 //center
+      },
+      subtitle: {
+        text: 'Collision Risk',
+        x: -20
+      },
+      yAxis: {
+        title: {
+          text: "Collision Risk",
+          min: 0.0,
+          max: 2.0
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      series: [{
+        name: "Collision Risk",
+        data: [],
+        id: "cRisk",
+        tooltip: {
+            headerFormat: 'No : {point.key}<br/>'
+        }
+      },{
+          type: 'flags',
+          data: [],
+          color: Highcharts.getOptions().colors[0], // same as onSeries
+          fillColor: '#FF0000',
+          onSeries: 'cRisk',
+          width: 16,
+          style: { // text style
+              color: 'white'
+          },
+          states: {
+              hover: {
+                  fillColor: '#FF0000' // darker
+              }
+          }
+        },{
+            type: 'flags',
+            data: [],
+            color: Highcharts.getOptions().colors[0], // same as onSeries
+            fillColor: '#FFFF00',
+            onSeries: 'cRisk',
+            width: 16,
+            style: { // text style
+                color: 'white'
+            },
+            states: {
+                hover: {
+                    fillColor: '#FFFF00' // darker
+                }
+            }
+          }]
+    });
+  });
+}
+
+function setChart1_risk(data) {
+  var chart = $('#chartDiv1_risk').highcharts();
+  var categories = $.map(data, function(obj) {
+    return obj.PTIME;
+  });
+  var data = $.map(data, function(obj) {
+    return obj.COLLISIONRISKMEAN;
+  })
+  // chart.xAxis[0].setCategories(categories);
+  chart.series[0].setData(data);
+
+  var dager = [];
+  var moredager = [];
+  for(var i = 0;i<data.length;i++){
+    if(data[i]>1.6){
+      moredager.push({
+          x: i,
+          title: 'VD',
+          text: 'Vary Dangerous'
+      });
+    } else if(data[i]>1.2){
+      dager.push({
+          x: i,
+          title: 'D',
+          text: 'Dangerous'
+      });
+    }
+  }
+  /*
+  chart.series[1].setData([{
+      x: categories.indexOf("1350"),
+      title: 'D',
+      text: 'Vary Dangerous'
+  }, {
+      x: categories.indexOf("1835"),
+      title: 'D',
+      text: 'Vary Dangerous'
+  }]);*/
+  chart.series[1].setData(moredager);
+  chart.series[2].setData(dager);
+  chart.xAxis[0].update({
+    labels: {
+      formatter: function() {
+        return categories[this.value];
+      }
+    }
+  }, true);
 }
 
 function getChart2() {
   $('#chartDiv2').highcharts({
     chart: {
-      type: 'boxplot'
+        type: 'boxplot'
     },
+
     title: {
-      text: 'Highcharts Box Plot Example'
+        text: 'Highway Prediction Error'
     },
+
     legend: {
-      enabled: false
+        enabled: false
     },
+
     xAxis: {
-      categories: ['1', '2', '3', '4', '5'],
-      title: {
-        text: 'Experiment No.'
-      }
-    },
-    yAxis: {
-      title: {
-        text: 'Observations'
-      },
-      plotLines: [{
-        value: 932,
-        color: 'red',
-        width: 1,
-        label: {
-          text: 'Theoretical mean: 932',
-          align: 'center',
-          style: {
-            color: 'gray'
-          }
+        //categories: ['1', '2', '3', '4', '5'],
+        title: {
+            text: 'CTIME'
         }
-      }]
     },
+
+    yAxis: {
+        title: {
+            text: 'DIFFERENCE'
+        }
+    },
+
     series: [{
-      name: 'Observations',
-      data: [
-        [760, 801, 848, 895, 965],
-        [733, 853, 939, 980, 1080],
-        [714, 762, 817, 870, 918],
-        [724, 802, 806, 871, 950],
-        [834, 836, 864, 882, 910]
-      ],
-      tooltip: {
-        headerFormat: '<em>Experiment No {point.key}</em><br/>'
-      }
-    }, {
-      name: 'Outlier',
-      color: Highcharts.getOptions().colors[0],
-      type: 'scatter',
-      data: [ // x, y positions where 0 is the first category
-        [0, 644],
-        [4, 718],
-        [4, 951],
-        [4, 969]
-      ],
-      marker: {
-        fillColor: 'white',
-        lineWidth: 1,
-        lineColor: Highcharts.getOptions().colors[0]
-      },
-      tooltip: {
-        pointFormat: 'Observation: {point.y}'
-      }
+        name: 'DIFFERENCE PSPEED TO ASPEED',
+        data: [
+            [760, 801, 848, 895, 965],
+            [733, 853, 939, 980, 1080],
+            [714, 762, 817, 870, 918],
+            [724, 802, 806, 871, 950],
+            [834, 836, 864, 882, 910]
+        ],
+        tooltip: {
+            headerFormat: 'CTIME : {point.key}<br/>'
+        }
     }]
   });
+}
+
+function setChart2(data){
+  var chart = $('#chartDiv2').highcharts();
+  var categories = [];
+  var baskets = {};
+  $.each(data, function(idx, obj) {
+    if(!baskets[obj.CTIME]) {
+      baskets[obj.CTIME] = [];
+      categories.push(obj.CTIME);
+    }
+    baskets[obj.CTIME].push(obj.PSPEED - obj.ASPEED);
+  });
+  var array = [];
+  $.each(baskets, function(key, arr) {
+    var idx = categories.indexOf("" + key);
+    array[idx] = arr;
+  });
+  var bValue = getBoxVaule(array);
+//  debugger
+  // chart.xAxis[0].setCategories(categories);
+  chart.series[0].setData(bValue);
 }
 
 function getChart3() {
   $('#chartDiv3').highcharts({
     chart: {
-      type: 'boxplot'
+        type: 'boxplot'
     },
+
     title: {
-      text: 'Highcharts Box Plot Example'
+        text: 'Arterial Prediction Error'
     },
+
     legend: {
-      enabled: false
+        enabled: false
     },
+
     xAxis: {
-      categories: ['1', '2', '3', '4', '5'],
-      title: {
-        text: 'Experiment No.'
-      }
-    },
-    yAxis: {
-      title: {
-        text: 'Observations'
-      },
-      plotLines: [{
-        value: 932,
-        color: 'red',
-        width: 1,
-        label: {
-          text: 'Theoretical mean: 932',
-          align: 'center',
-          style: {
-            color: 'gray'
-          }
+        //categories: ['1', '2', '3', '4', '5'],
+        title: {
+            text: 'CTIME'
         }
-      }]
     },
+
+    yAxis: {
+        title: {
+            text: 'DIFFERENCE'
+        }
+    },
+
     series: [{
-      name: 'Observations',
-      data: [
-        [760, 801, 848, 895, 965],
-        [733, 853, 939, 980, 1080],
-        [714, 762, 817, 870, 918],
-        [724, 802, 806, 871, 950],
-        [834, 836, 864, 882, 910]
-      ],
-      tooltip: {
-        headerFormat: '<em>Experiment No {point.key}</em><br/>'
-      }
-    }, {
-      name: 'Outlier',
-      color: Highcharts.getOptions().colors[0],
-      type: 'scatter',
-      data: [ // x, y positions where 0 is the first category
-        [0, 644],
-        [4, 718],
-        [4, 951],
-        [4, 969]
-      ],
-      marker: {
-        fillColor: 'white',
-        lineWidth: 1,
-        lineColor: Highcharts.getOptions().colors[0]
-      },
-      tooltip: {
-        pointFormat: 'Observation: {point.y}'
-      }
+        name: 'DIFFERENCE PSPEED TO ASPEED',
+        data: [
+            [760, 801, 848, 895, 965],
+            [733, 853, 939, 980, 1080],
+            [714, 762, 817, 870, 918],
+            [724, 802, 806, 871, 950],
+            [834, 836, 864, 882, 910]
+        ],
+        tooltip: {
+            headerFormat: 'CTIME : {point.key}<br/>'
+        }
     }]
   });
+}
+
+function setChart3(data){
+  var chart = $('#chartDiv3').highcharts();
+  var categories = [];
+  var baskets = {};
+  $.each(data, function(idx, obj) {
+    if(!baskets[obj.CTIME]) {
+      baskets[obj.CTIME] = [];
+      categories.push(obj.CTIME);
+    }
+    baskets[obj.CTIME].push(obj.PSPEED - obj.ASPEED);
+  });
+  var array = [];
+  $.each(baskets, function(key, arr) {
+    var idx = categories.indexOf("" + key);
+    array[idx] = arr;
+  });
+  var bValue = getBoxVaule(array);
+//  debugger
+  // chart.xAxis[0].setCategories(categories);
+  chart.series[0].setData(bValue);
+}
+
+
+function getBoxVaule(array){
+  var box_values = [];
+  for(var i=0; i<array.length;i++)
+  {
+    array[i].sort(function(a,b){
+      if(a>b) return 1;
+      else return -1;
+    });
+    var box_value = [];
+    var meanIndex = parseInt(array[i].length/2);
+    var lowerIndex = parseInt(array[i].length/4);
+    var upperIndex = 3*parseInt(array[i].length/4);
+    var minIndex = 0;
+    var maxIndex = array[i].length-1;
+    box_value.push(array[i][minIndex]);
+    box_value.push(array[i][lowerIndex]);
+    box_value.push(array[i][meanIndex]);
+    box_value.push(array[i][upperIndex]);
+    box_value.push(array[i][maxIndex]);
+    box_values.push(box_value);
+  }
+  return box_values;
 }
